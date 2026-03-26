9afaef4042ba97e53841fca6a4fe21df69d6ed20
noelbscht <noelbauschat@gmx.de>
2026-03-26 19:48:59 +0100

    feat: expand 'webservice' command to support serving the web interface service or all web services

--- src/main/java/net/libraya/gobdarchive/cli/commands/CommandWeb.java
+++ src/main/java/net/libraya/gobdarchive/cli/commands/CommandWeb.java
@@ -3,6 +3,7 @@ package net.libraya.gobdarchive.cli.commands;
 import net.libraya.gobdarchive.Main;
 import net.libraya.gobdarchive.cli.Command;
 import net.libraya.gobdarchive.service.api.API;
+import net.libraya.gobdarchive.service.web.WebServer;
 
 public class CommandWeb extends Command {
 
@@ -14,6 +15,8 @@ public class CommandWeb extends Command {
             <mode> --serve, --routes
             
             modes:
+            	full: run http + api (if enabled in .env)
+            	http: run web interface only
             	api:  run application interface only
             """
         );
@@ -36,7 +39,7 @@ public class CommandWeb extends Command {
         
         
         switch (mode) {
-        	case "api":// application interface
+        	case "api": {// application interface
         		API api = new API();
         		
         		if (parameter.equalsIgnoreCase("--serve")) {
@@ -52,9 +55,51 @@ public class CommandWeb extends Command {
             		return true;
                 }
         		break;
-    		default:
+        	}
+        	case "http": {// web interface
+        		WebServer http = new WebServer();
+        		
+        		if (parameter.equalsIgnoreCase("--serve")) {
+        			try {
+        				Main.getServicehandler().register(http);
+                		Main.getServicehandler().initialize();
+                	} catch (Exception e) {
+                        System.err.println("Failed to serve web service: " + e.getMessage());
+                        return false;
+                    }
+        		} else if (parameter.equalsIgnoreCase("--routes")) {
+        			http.sendRoutes();
+        			return true;
+        		}
+        		break;
+        	}
+        	case "full": {// api and web interface
+        		API api = new API();
+                WebServer http = new WebServer();
+
+        		if (parameter.equalsIgnoreCase("--serve")) {
+        			try {
+        				Main.getServicehandler().register(api);
+        				Main.getServicehandler().register(http);
+
+        				Main.getServicehandler().initialize();
+                        return true;
+
+                    } catch (Exception e) {
+                        System.err.println("Failed to start full web service: " + e.getMessage());
+                        if (e.getMessage().startsWith("Address already in use")) {
+                        	return true;
+                        }
+                        return false;
+                    }
+
+    			}
+        		break;
+        	}
+    		default: {
     			System.out.println("Invalid web service selected.");
     			break;
+    		}
         }
         return false;  
     }
