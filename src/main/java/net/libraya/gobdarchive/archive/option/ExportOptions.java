05e196190f0a0ddc8a7a903a1fd0cf64e7d2d020
noelbscht <noelbauschat@gmx.de>
2026-04-05 17:54:51 +0200

    feat(archive): add JSONObject constructor to ExportOptions and QueryFilterOptions classes for simpler object creation.

--- src/main/java/net/libraya/gobdarchive/archive/option/ExportOptions.java
+++ src/main/java/net/libraya/gobdarchive/archive/option/ExportOptions.java
@@ -1,11 +1,15 @@
 package net.libraya.gobdarchive.archive.option;
 
+import org.json.JSONObject;
+
 public class ExportOptions {
 	
     public boolean zip = true;
     public boolean includeAuditLog = true;
     
-    public ExportOptions() {
+    public ExportOptions(JSONObject input) {
+    	this.zip = input.optBoolean("zip");
+    	this.zip = input.optBoolean("include_audit_log");
     }
 }
 
--- src/main/java/net/libraya/gobdarchive/archive/option/QueryFilterOptions.java
+++ src/main/java/net/libraya/gobdarchive/archive/option/QueryFilterOptions.java
@@ -2,18 +2,39 @@ package net.libraya.gobdarchive.archive.option;
 
 import java.time.LocalDate;
 
+import org.json.JSONObject;
+
 import net.libraya.gobdarchive.archive.EntryType;
 
 public class QueryFilterOptions {
-	public EntryType type;
-	public LocalDate from;
-	public LocalDate to;
-	public String customKey;
-	public String customValue;
-	public String hash;
-	public String archiveId;
-	
-	public QueryFilterOptions() {
-		
-	}
+
+    public EntryType type;
+    public LocalDate from;
+    public LocalDate to;
+    public String customKey;
+    public String customValue;
+    public String hash;
+    public String archiveId;
+
+    public QueryFilterOptions(JSONObject input) {
+        if (input != null) {
+
+            String typeStr = normalize(input.optString("entry_type"));
+            String fromStr = normalize(input.optString("from"));
+            String toStr   = normalize(input.optString("to"));
+
+            this.type = typeStr != null ? EntryType.valueOf(typeStr) : null;
+            this.from = fromStr != null ? LocalDate.parse(fromStr) : null;
+            this.to   = toStr   != null ? LocalDate.parse(toStr)   : null;
+
+            this.customKey   = normalize(input.optString("custom_key"));
+            this.customValue = normalize(input.optString("custom_value"));
+            this.hash        = normalize(input.optString("hash"));
+            this.archiveId   = normalize(input.optString("archive_id"));
+        }
+    }
+
+    private String normalize(String s) {
+        return (s == null || s.isBlank()) ? null : s;
+    }
 }
