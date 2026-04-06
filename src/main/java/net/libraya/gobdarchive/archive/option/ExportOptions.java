9000d411629d673eef9c684f65a6984151d33288
noelbscht <noelbauschat@gmx.de>
2026-04-06 14:58:19 +0200

    fix(archive): interpret include_audit_log option correctly
    fixes incorrect interpretation of include_audit_log and introduces a
    DATEV option for future processing.

--- src/main/java/net/libraya/gobdarchive/archive/option/ExportOptions.java
+++ src/main/java/net/libraya/gobdarchive/archive/option/ExportOptions.java
@@ -4,12 +4,14 @@ import org.json.JSONObject;
 
 public class ExportOptions {
 	
-    public boolean zip = true;
+	public boolean zip = true;
     public boolean includeAuditLog = true;
+    public boolean interpretDATEV; 
     
     public ExportOptions(JSONObject input) {
     	this.zip = input.optBoolean("zip");
-    	this.zip = input.optBoolean("include_audit_log");
+    	this.includeAuditLog = input.optBoolean("include_audit_log");
+    	this.interpretDATEV = false; //todo:: optString interpret_datev
     }
 }
 
